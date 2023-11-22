d69774de8de10dd95ca02904178d350b0d6bc637
Tom Gardiner <tom.gardiner7@gmail.com>
2023-11-23 07:44:19 +1100

    Add reproducable error when using 1.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ subprojects {
     dependencies {
         val junitVersion = "5.10.1"
         // TODO try and fix issue with 1.7.3, which relates to classpath -  potentially being caused by build process
-        val kotlinCoroutinesVersion = "1.6.4"
+        val kotlinCoroutinesVersion = "1.7.3"
 
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${kotlinCoroutinesVersion}")
         implementation("org.junit.jupiter:junit-jupiter:${junitVersion}")
