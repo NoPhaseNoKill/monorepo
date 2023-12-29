f16b78c01f301eaabb367f944356006d0a65b07b
Tom Gardiner <tom.gardiner7@gmail.com>
2023-12-30 08:21:09 +1100

    Ensure composite builds are included when running the taskAll tasks

--- new/toolset/jvm/build.gradle.kts
+++ new/toolset/jvm/build.gradle.kts
@@ -1,6 +1,5 @@
 import java.util.*
 
-
 registerAllTask("build")
 registerAllTask("clean")
 
@@ -10,7 +9,7 @@ val localTaskGroup = "Local"
  * Reduces the noise in the exposed tasks, by simply doing the equivalent of a task at root (ie ./gradlew clean) behind
  * a scoped set of tasks in a 'group'.
  * registerAllTask("build") will register a task named 'buildAll',
- * that iterates over each of the subprojects and ensures :build is run on them.
+ * that iterates over each of the subprojects and composite builds and ensures :build is run on EACH them.
  */
 fun registerAllTask(
     taskName: String,
@@ -23,8 +22,31 @@ fun registerAllTask(
         description = "$taskDescription all subprojects/composite builds"
         additionalActions()
 
-        dependsOn(getTasksByName(taskName, true).map { it.path })
+        registerTaskEquivalents(this, taskName)
     }
+}
+
+fun registerTaskEquivalents(task: Task, taskName: String) {
+    // Composite builds are not a part of the tasks like subprojects and need to be 'manually' handled
+    task
+        .dependsOn(
+            gradle
+                .includedBuild("build-logic")
+                .task(":plugins:${taskName}")
+                .also {
+                    println("Included composite build task:  :build-logic:plugins:${taskName}")
+                }
+        )
+
+    // subprojects
+    task
+        .dependsOn(
+            getTasksByName(taskName, true)
+                .map { subproject ->
+                    println("Included subproject build task: ${subproject.path}")
+                    subproject.path
+                }
+        )
 }
 
 tasks.named<TaskReportTask>("tasks") {
