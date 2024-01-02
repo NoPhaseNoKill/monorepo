85083c2c71092efd89eede977b83d03fb3b420aa
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-03 01:58:06 +1100

    Shorten the dependency declaration of analysis plugin

--- new/toolset/jvm/build-logic/plugins/build.gradle.kts
+++ new/toolset/jvm/build-logic/plugins/build.gradle.kts
@@ -3,6 +3,5 @@ plugins {
 }
 
 dependencies {
-    // TODO move to platforms
-    implementation("com.autonomousapps.dependency-analysis:com.autonomousapps.dependency-analysis.gradle.plugin:1.28.0")
+    implementation("com.autonomousapps:dependency-analysis-gradle-plugin:1.28.0")
 }
