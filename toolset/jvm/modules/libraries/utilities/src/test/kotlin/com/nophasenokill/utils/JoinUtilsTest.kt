469fb8491bc83c4d6f3654cbcf17ea3342aa8fee
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-11 08:52:33 +1100

    Add in basic task snapshot

---
+++ toolset/jvm/modules/libraries/utilities/src/test/kotlin/com/nophasenokill/utils/JoinUtilsTest.kt
@@ -0,0 +1,14 @@
+package com.nophasenokill.utils
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class JoinUtilsTest {
+
+    @Test
+    fun `placeholder to check task outputs`() {
+        val one = 1
+        val three = 3
+        assertEquals(one + three, 4)
+    }
+}
--- toolset/jvm/settings.gradle.kts
+++ toolset/jvm/settings.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.gson.GsonBuilder
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -35,4 +37,76 @@ buildCache {
         isEnabled = true
         isPush = true
     }
+}
+
+data class TaskSnapshot(
+    val projectName: String,
+    val taskName: String,
+    val taskInputs: List<TaskSnapshotInput>,
+    val taskOutputs: List<TaskSnapshotOutput>,
+) {
+    override fun toString(): String {
+        return """
+            {
+              "project_name": "${this.projectName}",
+              "task_name": "${this.taskName}",
+              "task_inputs": ${taskInputs.joinToString(",\n", "[\n", "\n]") { "{ \"name\": \"${it.name}\", \"value\": \"${it.value}\" }"  } },
+              "task_outputs": ${taskOutputs.joinToString(",\n", "[\n", "\n]") { "{ \"name\": \"${it.name}\", \"path\": \"${it.path}\" }"  } }
+            }
+        """.trimIndent()
+    }
+}
+
+data class TaskSnapshotInput(
+    val name: String,
+    val value: Any?,
+)
+
+data class TaskSnapshotOutput(
+    val name: String,
+    val path: String,
+)
+
+gradle.taskGraph.whenReady {
+    val all = this.allTasks
+    val taskSnapshots = all.map { task: Task ->
+        val taskInputs = task.inputs.properties.entries.map {
+            TaskSnapshotInput(it.key, it.value)
+        }
+
+        val taskOutputs: List<TaskSnapshotOutput> = task.outputs.files.asFileTree.files.map { file ->
+            TaskSnapshotOutput(file.name, file.path)
+        }.sortedByDescending { it.name }
+
+        TaskSnapshot(
+            task.project.name,
+            task.name,
+            taskInputs,
+            taskOutputs
+        )
+
+    }
+
+    val unformatted = File("${rootDir}/snapshots/unformatted-tasks.json")
+    val formatted = File("${rootDir}/snapshots/formatted-tasks.json")
+
+    unformatted.bufferedWriter().use { writer ->
+        writer.append("{ \"tasks\": [ ")
+        taskSnapshots.forEachIndexed { index, taskSnapshot ->
+            writer.append(taskSnapshot.toString())
+            if (index != taskSnapshots.size - 1) {
+                writer.append(",")
+            }
+        }
+
+
+        writer.append("\n]\n}")
+    }.close()
+
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    val jsonContent = unformatted.readText()
+    val jsonElement = gson.fromJson(jsonContent, Any::class.java)
+    val formattedJson = gson.toJson(jsonElement)
+    formatted.writeText(formattedJson)
+    unformatted.delete()
 }
