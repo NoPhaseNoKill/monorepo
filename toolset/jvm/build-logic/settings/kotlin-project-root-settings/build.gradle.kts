d0a5c5bdba9d4885d782b4f9c3d5423d443562f5
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-11 15:05:42 +1100

    Remove unnecessary repos for settings

--- toolset/jvm/build-logic/settings/kotlin-project-root-settings/build.gradle.kts
+++ toolset/jvm/build-logic/settings/kotlin-project-root-settings/build.gradle.kts
@@ -1,16 +1,9 @@
 plugins {
-    // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
     `kotlin-dsl`
 }
 
-repositories {
-    // Use the plugin portal to apply community plugins in convention plugins.
-    gradlePluginPortal()
-    mavenCentral()
-}
-
 dependencies {
+    implementation(platform("com.nophasenokill.platform:base-platform"))
     implementation("org.gradle.toolchains:foojay-resolver")
     implementation("com.gradle.enterprise:com.gradle.enterprise.gradle.plugin")
-    implementation(platform("com.nophasenokill.platform:base-platform"))
 }
--- toolset/jvm/build-logic/settings/kotlin-project-root-settings/src/main/kotlin/kotlin-project-root-settings.settings.gradle.kts
+++ toolset/jvm/build-logic/settings/kotlin-project-root-settings/src/main/kotlin/kotlin-project-root-settings.settings.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention")
     id("com.gradle.enterprise")
-
 }
