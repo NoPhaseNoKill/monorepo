a330d6757a8a487e7fabdb86bef8e818660f6499
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-11 16:35:17 +1100

    Update plugin to fail build if any dep issues found

--- toolset/jvm/build.gradle.kts
+++ toolset/jvm/build.gradle.kts
@@ -2,3 +2,15 @@ plugins {
     `kotlin-dsl` apply false
     id("com.autonomousapps.dependency-analysis") version "1.28.0"
 }
+
+dependencyAnalysis {
+    issues {
+        // configure for all projects
+        all {
+            // set behavior for all issue types
+            onAny {
+                severity("fail")
+            }
+        }
+    }
+}
