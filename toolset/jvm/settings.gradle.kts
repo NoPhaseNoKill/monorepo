5039254f00260bc1eca39509f60e3bcf7d6a614a
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-14 00:24:43 +1100

    Add back in build scan obfuscation of PID

--- toolset/jvm/settings.gradle.kts
+++ toolset/jvm/settings.gradle.kts
@@ -8,10 +8,18 @@ plugins {
 
 rootProject.name = "jvm"
 
+val obfuscatedValue = "obfuscated"
+
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
+
+        obfuscation {
+            username { obfuscatedValue }
+            ipAddresses { addresses -> addresses.map { _ -> "${obfuscatedValue}.${obfuscatedValue}.${obfuscatedValue}.${obfuscatedValue}" } }
+            hostname { obfuscatedValue }
+        }
     }
 }
 
