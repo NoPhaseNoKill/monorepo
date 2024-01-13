7c21a98fbfd26482143602114866e4783429a2e3
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-14 00:34:46 +1100

    Fix broken configuration cache warning

--- toolset/jvm/settings.gradle.kts
+++ toolset/jvm/settings.gradle.kts
@@ -8,17 +8,26 @@ plugins {
 
 rootProject.name = "jvm"
 
-val obfuscatedValue = "obfuscated"
 
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
 
+        /*
+            If you try to abstract these strings to a 'val' outside gradleEnterprise lambda, you get errors
+            such as below:
+
+            4 problems were found storing the configuration cache.
+            - Gradle runtime: cannot serialize Gradle script object references as these are not supported with the configuration cache.
+              See https://docs.gradle.org/8.5/userguide/configuration_cache.html#config_cache:requirements:disallowed_types
+                ...
+                ...
+         */
         obfuscation {
-            username { obfuscatedValue }
-            ipAddresses { addresses -> addresses.map { _ -> "${obfuscatedValue}.${obfuscatedValue}.${obfuscatedValue}.${obfuscatedValue}" } }
-            hostname { obfuscatedValue }
+            username { "OBFUSCATED_USERNAME" }
+            ipAddresses { addresses -> addresses.map { _ -> "OBFUSCATED_IP_ADDRESS" } }
+            hostname { "OBFUSCATED_HOSTNAME" }
         }
     }
 }
