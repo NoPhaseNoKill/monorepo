5bc7fb828546cc6b83ba02f888e896fc1dd019a5
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-14 18:37:17 +1100

    Rename some of the root settings files to make more sense

--- toolset/jvm/build-logic/settings/kotlin-project-root-settings/src/main/kotlin/kotlin-project-root-settings.settings.gradle.kts
+++ toolset/jvm/build-logic/settings/kotlin-project-root-settings/src/main/kotlin/kotlin-project-root-settings.settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.gradle.enterprise")
-    id("kotlin-project-project-structure")
+    id("kotlin-project-root-project-structure")
     id("kotlin-project-root-repositories")
 }
