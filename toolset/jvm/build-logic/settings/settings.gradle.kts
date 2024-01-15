6faa9bf29f6c35fb5fc3ab8796b42d2446bc552b
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-15 20:55:11 +1100

    Fail on projects that declare repositories

--- toolset/jvm/build-logic/settings/settings.gradle.kts
+++ toolset/jvm/build-logic/settings/settings.gradle.kts
@@ -13,6 +13,9 @@
 dependencyResolutionManagement {
     // forces underlying modules to use our own convention plugins
     repositories.gradlePluginPortal()
+
+    // See: https://docs.gradle.org/current/userguide/declaring_repositories.html#ex-enforcing-settings-repositories
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
 }
 
 rootProject.name = "settings"
