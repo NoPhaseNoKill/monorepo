6c2691bbd2dedfcebf2afaaccb5740a94c37431e
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-19 15:26:01 +1100

    Move project structure to root settings file

--- toolset/jvm/build-logic/settings/kotlin-project-root-settings/src/main/kotlin/kotlin-project-root-settings.settings.gradle.kts
+++ toolset/jvm/build-logic/settings/kotlin-project-root-settings/src/main/kotlin/kotlin-project-root-settings.settings.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.gradle.enterprise")
     id("org.gradle.toolchains.foojay-resolver-convention")
-    id("kotlin-project-root-project-structure")
     id("kotlin-project-root-repositories")
 }
--- toolset/jvm/settings.gradle.kts
+++ toolset/jvm/settings.gradle.kts
@@ -38,3 +38,14 @@ buildCache {
         isPush = true
     }
 }
+
+// Dynamically includes top level directories within each of the modules' sub-folders
+val directories = setOf("applications", "libraries")
+directories.forEach { dir ->
+    rootDir
+            .resolve("modules/${dir}")
+            .listFiles { file -> file.isDirectory && !file.isHidden }
+            ?.forEach {
+                include("modules:$dir:${it.name}")
+            }
+}
