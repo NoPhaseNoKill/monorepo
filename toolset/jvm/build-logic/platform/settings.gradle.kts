67516785e8f29c1081dce5f1821148355c32bed9
Tom Gardiner <tom.gardiner7@gmail.com>
2024-01-19 15:15:56 +1100

    Move platform away from settings

--- toolset/jvm/build-logic/platform/settings.gradle.kts
+++ toolset/jvm/build-logic/platform/settings.gradle.kts
@@ -1,9 +1,20 @@
 pluginManagement {
-    includeBuild("../settings")
+    repositories {
+        gradlePluginPortal()
+        includeBuild("../plugins")
+    }
+}
+
+dependencyResolutionManagement {
+    // forces underlying modules to use our own convention plugins
+    repositories.gradlePluginPortal()
+
+    // See: https://docs.gradle.org/current/userguide/declaring_repositories.html#ex-enforcing-settings-repositories
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
 }
 
 plugins {
-    id("kotlin-project-root-settings")
+//    id("kotlin-project-root-settings")
 }
 
 rootProject.name = "platform"
