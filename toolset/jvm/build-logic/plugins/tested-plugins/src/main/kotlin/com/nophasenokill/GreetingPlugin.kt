571865a7048eb06e1c47702c1af31bc224c463a9
Tom Gardiner <tom.gardiner7@gmail.com>
2024-02-14 21:18:25 +1100

    Remove last created immediately task

--- toolset/jvm/build-logic/plugins/tested-plugins/src/main/kotlin/com/nophasenokill/GreetingPlugin.kt
+++ toolset/jvm/build-logic/plugins/tested-plugins/src/main/kotlin/com/nophasenokill/GreetingPlugin.kt
@@ -6,11 +6,11 @@ import org.gradle.api.Project
 
 class GreetingPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-
+        
         with(project) {
-            plugins.apply(CommonsTestedPlugin::class.java)
+            tasks.register("taskInsideGreetingPlugin") {
+                plugins.apply(CommonsTestedPlugin::class.java)
 
-            task("taskInsideGreetingPlugin") {
                 this.doLast {
                     logger.lifecycle("Hello from plugin 'greeting-plugin'")
                 }
