0f1161a82bf25b1c00b06a445a3ac1aa5d461107
Tom Gardiner <tom.gardiner7@gmail.com>
2024-02-18 20:41:36 +1100

    Add caching for MD5 tasks to reduce build times

--- toolset/jvm/build-logic/plugins/source-file-hashing-plugin/src/main/kotlin/com/nophasenokill/GenerateMD5.kt
+++ toolset/jvm/build-logic/plugins/source-file-hashing-plugin/src/main/kotlin/com/nophasenokill/GenerateMD5.kt
@@ -2,17 +2,25 @@ package com.nophasenokill
 
 import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.io.FileUtils
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputFile
 import org.gradle.workers.WorkAction
+import java.io.File
 import java.io.FileInputStream
 import java.io.InputStream
 
 
 // Do not implement the getParameters() method - Gradle will inject this at runtime
 abstract class GenerateMD5 : WorkAction<MD5WorkParameters> {
+
+    @InputFile
+    val sourceFile: File = parameters.sourceFile.asFile.get()
+
+    @OutputFile
+    val md5File: File = parameters.mD5File.asFile.get()
+
     override fun execute() {
         try {
-            val sourceFile = parameters.sourceFile.asFile.get()
-            val md5File = parameters.mD5File.asFile.get()
             val stream: InputStream = FileInputStream(sourceFile)
             println("Generating MD5 for " + sourceFile.name + "...")
             // Artificially make this task slower.
