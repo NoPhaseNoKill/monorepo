2658a23db9f74d25a34a90eed1f107a657a436c9
Tom Gardiner <tom.gardiner7@gmail.com>
2024-03-02 22:02:59 +1100

    WIP - Configuration Avoidance Discovery - Reduce created during configuration tasks from 53->33 for gradlew test task

--- toolset/jvm/build-logic/plugins/settings.gradle.kts
+++ toolset/jvm/build-logic/plugins/settings.gradle.kts
@@ -17,22 +17,22 @@ rootProject.name = "plugins"
 
 includeBuild("../platform")
 
-include("capability-conflict-avoidance-plugin")
-include("classpath-collision-detector-plugin")
-include("tested-plugins")
-include("dependency-analysis-project")
-include("dependency-analysis-platform")
-include("source-file-hashing-plugin")
-include("performance-metrics-plugin")
-include("dependency-analysis-plugin")
+// include("capability-conflict-avoidance-plugin")
+// include("classpath-collision-detector-plugin")
+// include("tested-plugins")
+// include("dependency-analysis-project")
+// include("dependency-analysis-platform")
+// include("source-file-hashing-plugin")
+// include("performance-metrics-plugin")
+// include("dependency-analysis-plugin")
 
 include("base-plugin")
 include("my-kotlin-plugin")
 include("library-plugin")
 include("application-plugin")
-include("kotlin-app-plugin")
+// include("kotlin-app-plugin")
 include("junit-test-plugin")
-include("kotlin-lib-plugin")
+// include("kotlin-lib-plugin")
 
 
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
