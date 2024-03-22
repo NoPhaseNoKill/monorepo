0184cad49f8bb66cba4557f668a5f8c0761cde9d
Tom Gardiner <tom.gardiner7@gmail.com>
2024-03-23 09:34:04 +1100

    Add back in configuration cache task

--- toolset/jvm/build.gradle.kts
+++ toolset/jvm/build.gradle.kts
@@ -1,3 +1,55 @@
+import java.util.*
+
+val currentConfigurationCacheValueTask = tasks.register<CurrentConfigurationCacheValueTask>("currentConfigurationCacheValueTask") {
+
+    configurationCacheFromPropertiesValue.set(providers.gradleProperty("org.gradle.configuration-cache").getOrElse("false"))
+    configurationCacheInfoFile.set(layout.buildDirectory.file("generated-resources/configurationCacheFromPropertiesValue.properties"))
+
+    outputs.upToDateWhen { false }
+    outputs.cacheIf { false }
+
+    doLast {
+
+        val indent = "      "
+        val outputText = configurationCacheInfoFile.get().asFile.readText()
+        val isConfigurationCacheOn = outputText.contains("isConfigurationCacheOn=true")
+        logger.lifecycle("$indent *** REMINDER ***\n $indent $indent | Configuration cache key is set to: ~~~$isConfigurationCacheOn~~~\n $indent $indent | You will experience: ")
+
+        if(isConfigurationCacheOn) {
+            logger.lifecycle("$indent $indent $indent| -Issues With Running Dependencies Task In Intellij")
+            logger.lifecycle("$indent $indent $indent| -But Tests Will Also Be Much Faster")
+        } else {
+            logger.lifecycle("$indent $indent $indent| -Significant Slowness")
+            logger.lifecycle("$indent $indent $indent| -But You Will Be Able To Run Intellij Dependencies Task")
+        }
+
+        logger.lifecycle("$indent *** SEE GRADLE.PROPERTIES FOR MORE DETAILS ***")
+    }
+}
+
+
+abstract class CurrentConfigurationCacheValueTask : DefaultTask() {
+    @get:Input
+    abstract val configurationCacheFromPropertiesValue: Property<String>
+
+    @get:OutputFile
+    abstract val configurationCacheInfoFile: RegularFileProperty
+
+    @TaskAction
+    fun writeVersionInfo() {
+        val properties = Properties()
+
+        val value = configurationCacheFromPropertiesValue.get()
+        val isConfigurationCacheOn = value == "true"
+
+        properties.setProperty("isConfigurationCacheOn", isConfigurationCacheOn.toString())
+
+        configurationCacheInfoFile.get().asFile.outputStream().use { out ->
+            properties.store(out, null)
+        }
+    }
+}
+
 
 tasks.register("check") {
     group = "verification"
@@ -7,4 +59,6 @@ tasks.register("check") {
     dependsOn(gradle.includedBuild("modules").task(":libraries:utilities:check"))
     dependsOn(gradle.includedBuild("modules").task(":applications:app:check"))
     dependsOn(gradle.includedBuild("modules").task(":applications:accelerated-test-suite-runner:check"))
+
+    finalizedBy(currentConfigurationCacheValueTask)
 }
