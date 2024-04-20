309136d2bea8997e85e8fb9f90cdb408906d5b0c
Tom Gardiner <tom.gardiner7@gmail.com>
2024-04-21 00:59:35 +1000

    Exclude standalone plugin from application one which causes more tasks to be run than needed

--- toolset/jvm/composite-build/applications/application-one/build.gradle.kts
+++ toolset/jvm/composite-build/applications/application-one/build.gradle.kts
@@ -6,10 +6,7 @@ group = "com.nophasenokill"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    /*
-        Adds in the dependency for a plugin we rely on
-     */
-    implementation("com.nophasenokill.standalone-plugins:standalone-plugin-one")
+    
 }
 
 /*
