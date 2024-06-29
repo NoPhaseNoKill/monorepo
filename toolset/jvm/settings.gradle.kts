fdfd2b681551b90d8b4a03bbad5b6628285234aa
Tom Gardiner <tom.gardiner7@gmail.com>
2024-06-30 04:29:05 +1000

    Remove unnecessary guff from root settings file

--- toolset/jvm/settings.gradle.kts
+++ toolset/jvm/settings.gradle.kts
@@ -1,40 +1,19 @@
 rootProject.name = "jvm"
 
 pluginManagement {
-    /*
-        Including these here allows us to access plugins and prevents them being unable to be
-        found.
-     */
     includeBuild("known-to-be-working-examples/plugins/root-settings-plugin")
-    includeBuild("known-to-be-working-examples/plugins/hash-source-plugin")
-    repositories {
-        gradlePluginPortal() // Allows local plugins to be found
-    }
 }
+
+includeBuild("known-to-be-working-examples/domain")
+includeBuild("known-to-be-working-examples/plugins/hash-source-plugin")
+include("known-to-be-working-examples:plugins:basic-plugin")
+include("known-to-be-working-examples:plugins:extended-plugin")
+
 
 dependencyResolutionManagement {
     rulesMode = RulesMode.FAIL_ON_PROJECT_RULES
-    repositories {
-        /*
-            Allows us to declare dependency on the plugin at the project level
-
-            Without this, the included project of :known-to-be-working-examples:plugins:extended-plugin, would get:
-
-            Could not determine the dependencies of task ':known-to-be-working-examples:plugins:extended-plugin:compileJava'.
-            > Could not resolve all task dependencies for configuration ':known-to-be-working-examples:plugins:extended-plugin:compileClasspath'.
-               > Could not find com.nophasenokill.hash-source-plugin:hash-source-plugin:0.1.local-dev.
-         */
-        includeBuild("known-to-be-working-examples/plugins/hash-source-plugin")
-        mavenCentral()
-        gradlePluginPortal()
-    }
 }
 
 plugins {
     id("com.nophasenokill.root-settings-plugin")
 }
-
-include("known-to-be-working-examples:plugins:basic-plugin")
-include("known-to-be-working-examples:plugins:extended-plugin")
-includeBuild("known-to-be-working-examples/domain")
-
