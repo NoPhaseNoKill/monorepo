2c856d2cf647591ffec570f85a205b631ed66c5b
Tom Gardiner <tom.gardiner7@gmail.com>
2024-07-30 05:14:16 +1000

    Make json comparator

--- toolset/jvm/standalone-projects/applications/build-tool-ui/src/main/kotlin/com/nophasenokill/Main.kt
+++ toolset/jvm/standalone-projects/applications/build-tool-ui/src/main/kotlin/com/nophasenokill/Main.kt
@@ -1,100 +1,85 @@
-package com.nophasenokill
-
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
-
-
-fun main() = application {
-
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Compose for Desktop",
-        state = rememberWindowState(width = 850.dp, height = 850.dp)
-    ) {
-        MaterialTheme {
-            Surface(modifier = Modifier.fillMaxSize()) {
-                MessageCard(
-                    msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-                )
-            }
-        }
-    }
-}
-
-data class Message(val author: String, val body: String)
-
+import org.json.JSONException
+import org.skyscreamer.jsonassert.JSONCompare
+import org.skyscreamer.jsonassert.JSONCompareMode
 
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource("profile_picture.png"),
-            contentDescription = null,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
+@Preview
+fun App() {
+    var result by remember { mutableStateOf("") }
+    var json1 by remember { mutableStateOf("") }
+    var json2 by remember { mutableStateOf("") }
+    val padding = 16.dp
 
-        // We keep track if the message is expanded or not in this
-        // variable
-        var isExpanded by remember { mutableStateOf(false) }
-        // surfaceColor will be updated gradually from one color to the other
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
-        )
+    MaterialTheme {
+        Column(
+            modifier = Modifier.padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(Modifier.padding(padding)) {
+                OutlinedTextField(
+                    value = json1,
+                    onValueChange = { json1 = it },
+                    label = { Text("JSON 1") },
+                    modifier = Modifier.fillMaxWidth(0.5F).fillMaxHeight(0.75F)
 
-
-        // We toggle the isExpanded variable when we click on this Column
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondary,
-                style = MaterialTheme.typography.h6
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                elevation = 1.dp,
-                // surfaceColor color will be changing gradually from primary to surface
-                color = surfaceColor,
-                // animateContentSize will change the Surface size gradually
-                modifier = Modifier.animateContentSize().padding(1.dp)
-
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    // If the message is expanded, we display all its content
-                    // otherwise we only display the first line
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.body2
                 )
+                Spacer(Modifier.size(padding))
+                OutlinedTextField(
+                    value = json2,
+                    onValueChange = { json2 = it },
+                    label = { Text("JSON 2") },
+                    modifier = Modifier.fillMaxWidth().fillMaxHeight(0.75F)
+                )
+            }
+            Row {
+                Button(onClick = {
+                    result = compareJSON(json1, json2)
+                }) {
+                    Text("Compare")
+                }
+            }
+            if (result.isNotBlank()) {
+                Row {
+                    OutlinedTextField(
+                        value = result,
+                        readOnly = true,
+                        onValueChange = {},
+                        label = { Text("Result") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
         }
     }
 }
 
+fun compareJSON(json1: String, json2: String): String {
+    return try {
+        val comparison = JSONCompare.compareJSON(json1, json2, JSONCompareMode.STRICT)
+        if (comparison.passed()) "✅ Same JSON" else "❗ Differences :\n ${comparison.message}"
+    } catch (e: JSONException) {
+        "❌ Format Error"
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication, title = "JSON comparator") {
+        App()
+    }
+}
