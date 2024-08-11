75f297bdab4c750ad07028169e4c72953eb2d788
Tom Gardiner <tom.gardiner7@gmail.com>
2024-08-11 22:58:24 +1000

    Add in gradle version, jvm args, build id

--- toolset/jvm/standalone-projects/applications/example-desktop-application/src/main/kotlin/com/nophasenokill/gradle/GradleToolingApi.kt
+++ toolset/jvm/standalone-projects/applications/example-desktop-application/src/main/kotlin/com/nophasenokill/gradle/GradleToolingApi.kt
@@ -2,35 +2,14 @@ package com.nophasenokill.gradle
 
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.ProjectConnection
+import org.gradle.tooling.model.build.BuildEnvironment
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.PrintStream
 import java.nio.file.Paths
 
+
 object GradleToolingApi {
-    fun runTestsUltraFast(connection: ProjectConnection): String {
-        val startTime = System.currentTimeMillis()
-
-        val outputStream = ByteArrayOutputStream()
-
-        connection
-            .newBuild()
-            .forTasks("testAll")
-            .setStandardOutput(PrintStream(outputStream))
-            .withArguments("--scan", "--console=plain")
-            .run()
-
-        // Measure end time and calculate the duration
-        val endTime = System.currentTimeMillis()
-        val duration = (endTime - startTime)
-
-        println("Task executed in $duration ms")
-
-        val output = outputStream.toString()
-        println("Build Output:\n$output")
-        return output
-
-    }
 
     fun getConnector(): GradleConnector {
         val rootJvmPath = Paths.get("").toAbsolutePath().parent.parent.parent.toString()
@@ -40,7 +19,33 @@ object GradleToolingApi {
         return connector
     }
 
-    fun runTestSuite(connector: ProjectConnection): String {
-        return runTestsUltraFast(connector)
+    fun runTestSuite(connection: ProjectConnection): String {
+        val startTime = System.currentTimeMillis()
+
+        val outputStream = ByteArrayOutputStream()
+
+        connection
+            .newBuild()
+            .forTasks("testAll")
+            .setStandardOutput(PrintStream(outputStream))
+            .withArguments("--scan", "--console=plain" )
+            .run()
+
+        // Measure end time and calculate the duration
+        val endTime = System.currentTimeMillis()
+        val duration = (endTime - startTime)
+
+        println("Task executed in $duration ms")
+
+        val buildEnv = connection.getModel(BuildEnvironment::class.java)
+        var finalOutput = ""
+        finalOutput += "Gradle version: ${buildEnv.gradle.gradleVersion}\n"
+        finalOutput += "Java home: ${buildEnv.java.javaHome}\n"
+        finalOutput += "Build identifier: ${buildEnv.buildIdentifier.rootDir}\n"
+        finalOutput += "JVM args: [ \n${buildEnv.java.jvmArguments.sortedDescending().joinToString(",\n") { it }} \n]\n"
+        finalOutput += outputStream.toString() + "\n"
+
+        println("Build Output:\n$finalOutput")
+        return finalOutput
     }
 }
